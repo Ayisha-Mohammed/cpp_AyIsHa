6270ec43cb6a9a7e789ad0843d7ee5fc0ba6a628
Ayisha <ca20ayisharukshana.m@gmail.com>
2025-09-03 16:57:27 +0530

    updated

---
+++ even_nums.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+    int n;
+    cout<<"enter a number to check if it is even ";
+    cin>>n;
+
+    for(int i=0;i<=n;i++){
+        if(i%2==0){
+            cout<<n<<"is even";
+        }
+
+    }
+
+
+
+    return 0; 
+}
